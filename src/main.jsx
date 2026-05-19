13b5aac28629f556448aabc64480079329c7dcef
rsaniei1991 <rs_ir2005@yahoo.com>
2026-05-19 15:14:04 +0200

    fix bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+	// <StrictMode>
+	<App />,
+	// </StrictMode>,
 );
-
-// console.log("MAIN START");
-
-// const rootEl = document.getElementById("root");
-
-// console.log("ROOT ELEMENT:", rootEl);
-
-// rootEl.innerHTML = "<h1>DIRECT DOM WORKS</h1>";
